3de5bdcca6c11afa72b02adeeb065b3d606937cc
whrho <whrho@sparrowfasoo.com>
2022-04-21 19:33:12 +0900

    fixed typo

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -60,7 +60,7 @@ T_IDXS_LST = [index_function(idx, max_val=MAX_T, max_idx=N) for idx in range(N+1
 
 T_IDXS = np.array(T_IDXS_LST)
 T_DIFFS = np.diff(T_IDXS, prepend=[0.])
-vMIN_ACCEL = -3.5
+MIN_ACCEL = -3.5
 COMFORT_BRAKE = 2.5
 T_FOLLOW = 1.375 # 1.45
 STOP_DISTANCE = 7.75  # 6.5
