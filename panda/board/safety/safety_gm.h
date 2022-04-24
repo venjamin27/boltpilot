afa98a70aee6210f560d2a4c4c22b263dcab94ab
jc01rho <rkjnice@gmail.com>
2022-04-24 11:24:59 +0900

    fix typoes..

--- panda/board/safety/safety_gm.h
+++ panda/board/safety/safety_gm.h
@@ -260,7 +260,7 @@ static int gm_tx_hook(CANPacket_t *to_send, bool longitudinal_allowed) {
   return tx;
 }
 
-<<<<<<< HEAD
+
 static int gm_fwd_hook(int bus_num, CANPacket_t *to_fwd) {
 
 //  int bus_fwd = -1;
