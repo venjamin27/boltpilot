a35fff676921e6e8c74ff5740cabe0e015c43892
jc01rho <rkjnice@gmail.com>
2022-06-16 20:01:12 +0900

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -163,7 +163,7 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.torque.friction = 0.02
 
       ret.lateralTuning.torque.kd = 1.0
-      ret.lateralTuning.torque.deadzone = 0.01
+      #ret.lateralTuning.torque.deadzone = 0.01
 
 
 
