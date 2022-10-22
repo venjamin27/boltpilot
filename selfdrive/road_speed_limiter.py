32c2bfd9c7031566ccfc3fbffba2d029fe7b9624
jc01rho <rkjnice@gmail.com>
2022-10-22 20:00:05 +0900

    fixed logging

--- selfdrive/road_speed_limiter.py
+++ selfdrive/road_speed_limiter.py
@@ -80,7 +80,7 @@ class RoadLimitSpeedServer:
         self.gps_sm.update(0)
         if self.gps_sm.updated['gpsLocationExternal']:
           location = self.gps_sm['gpsLocationExternal']
-          print("location.accuracy :  " + str(location.accuracy))
+          print("location.timestamp :  " + str(location.timestamp))
           if location.accuracy < 20.:
             json_location = json.dumps({"location": [
               location.latitude,
