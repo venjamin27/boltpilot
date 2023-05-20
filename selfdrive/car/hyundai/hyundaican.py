11f043e0c293d0f998b835a66bbcee1322c3c3b9
ajouatom <ajouatom@gmail.com>
2023-05-20 12:59:52 +0900

    for KONA EV

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -142,8 +142,8 @@ def create_acc_commands_mix_scc(CP, packer, enabled, accel, upper_jerk, idx, hud
       scc12_accMode = 1
       scc14_accMode = 1
       stopReq = 1
-    comfortBandUpper = 1.0
-    comfortBandLower = 1.0
+    comfortBandUpper = 0.9 + accel * 0.2
+    comfortBandLower = 0.8 + accel * 0.2
     jerkUpperLimit = upper_jerk
     jerkLowerLimit = upper_jerk #5.0
   else:
