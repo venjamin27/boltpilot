82907ad729cd292f72a834cd40c647f23ed986da
jc01rho <rkjnice@gmail.com>
2023-05-25 20:08:07 +0900

    added regen_paddle_commnand for slowing down

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -155,6 +155,8 @@ class CarController:
 
           if not CC.longActive:
             pedal_gas = 0.0  # May not be needed with the enable param
+          elif pedal_gas < 0.075 :
+            can_sends.append(gmcan.create_regen_paddle_command(self.packer_pt, CanBus.POWERTRAIN))
 
           idx = (self.frame // 4) % 4
           can_sends.append(create_gas_interceptor_command(self.packer_pt, pedal_gas, idx))
