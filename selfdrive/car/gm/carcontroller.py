c3a8eb1d301a854bf28341936af7bcae13d7414c
whrho <whrho@sparrowfasoo.com>
2023-06-01 15:27:02 +0900

    fixed actuator_hystereses gap.

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -21,9 +21,9 @@ CAMERA_CANCEL_DELAY_FRAMES = 10
 MIN_STEER_MSG_INTERVAL_MS = 15
 
 
-def actuator_hystereses(final_pedal, pedal_steady):
+def actuator_hystereses(final_pedal, pedal_steady, pedal_hyst_gap=0.01):
   # hyst params... TODO: move these to VehicleParams
-  pedal_hyst_gap = 0.01    # don't change pedal command for small oscillations within this value
+      # don't change pedal command for small oscillations within this value
 
   # for small pedal oscillations within pedal_hyst_gap, don't change the pedal command
   if math.isclose(final_pedal, 0.0):
@@ -157,7 +157,7 @@ class CarController:
 
           # apply pedal hysteresis and clip the final output to valid values.
           self.pedalGasRaw_valueStore = pedal_gas
-          pedal_final, self.pedal_steady = actuator_hystereses(pedal_gas, self.pedal_steady)
+          pedal_final, self.pedal_steady = actuator_hystereses(pedal_gas, self.pedal_steady, 0.01 / 5.0)
           pedal_gas = clip(pedal_final, 0., 1.)
 
           if not CC.longActive:
