459fa12419cde90f4a6ad284cf8690c1365bde9b
whrho <whrho@sparrowfasoo.com>
2023-06-14 15:46:29 +0900

    old c basic style sprintf

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1660,15 +1660,15 @@ void DrawApilot::drawDebugText(UIState* s) {
     float vision_dist = lead_one.getProb() > .5 ? (lead_one.getX()[0] - 1.5) : 0;
 
     y += dy;
-    str.sprintf("Leadv3 d: %.1f v: %.1f \n", vision_dist, lead_one.getV()[0]);
+    sprintf(str,"Leadv3 d: %.1f v: %.1f", vision_dist, lead_one.getV()[0]);
     ui_draw_text(s, text_x, y, str, 35, COLOR_WHITE, BOLD, 0.0f, 0.0f);
 
     if (radar_lead_one.getStatus())  {
 
-      str.sprintf("radar: d: %.1f v: %.1f \n", radar_lead_one.getDRel(), radar_lead_one.getVRel());
+      sprintf(str,"radar: d: %.1f v: %.1f", radar_lead_one.getDRel(), radar_lead_one.getVRel());
 
     } else {
-      str.sprintf("radar: d: %.1f v: %.1f \n", 0.0, 0.0);
+      sprintf(str,"radar: d: %.1f v: %.1f", 0.0f, 0.0f);
     }
     y += dy;
     ui_draw_text(s, text_x, y, str, 35, COLOR_WHITE, BOLD, 0.0f, 0.0f);
