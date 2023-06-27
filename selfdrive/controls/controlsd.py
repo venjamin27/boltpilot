8670f099ee6d0753fa65826a991881b0d6be3cf7
whrho <whrho@sparrowfasoo.com>
2023-06-27 21:41:25 +0900

    fixed params err.

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -696,7 +696,7 @@ class Controls:
       # accel PID loop
       pid_accel_limits = self.CI.get_pid_accel_limits(self.CP, CS.vEgo, self.v_cruise_helper.v_cruise_kph * CV.KPH_TO_MS)
       t_since_plan = (self.sm.frame - self.sm.rcv_frame['longitudinalPlan']) * DT_CTRL
-      actuators.accel = self.LoC.update(CC.longActive, CS, long_plan, pid_accel_limits, t_since_plan, self.experimental_mode)
+      actuators.accel = self.LoC.update(CC.longActive, CS, long_plan, pid_accel_limits, t_since_plan, CC, self.experimental_mode)
 
       if len(long_plan.speeds):
         actuators.speed = long_plan.speeds[-1]
